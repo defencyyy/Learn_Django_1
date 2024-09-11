976ec13f09e1f8d83f7af3d93f807d1a57a6ba06
Cyrus <cysness@gmail.com>
2024-09-11 16:42:39 +0800

    Fixed database, added admin area for listing and user

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  description = models.CharField(blank=True)
+  phone = models.CharField(max_length=20)
+  email = models.CharField(max_length=50)
+  creation_date = models.DateTimeField(default=datetime.now, blank=True)
+  def __str__(self):
+    return self.last_name + ", " + self.first_name
